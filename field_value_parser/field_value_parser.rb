264af0e78d655176bd854db10e662b7e7756115d
Zimm <Romsssa@gmail.com>
2017-05-10 12:06:16 +0300

    Update process_values logic

--- field_value_parser/field_value_parser.rb
+++ field_value_parser/field_value_parser.rb
@@ -1,22 +1,17 @@
 require 'active_support/core_ext/string'
 
 def process_values(fields_with_values = {})
-	result = fields_with_values
+  fields_with_values.each do |key, value|
+    fields_with_values[key] = make_replaces value, key, fields_with_values
+  end
+end
 
-	# { text_1: 'text1_value [NUMBER_1]',  number_1: '2' }
-	# text_2: 'text2_value [TEXT_1]',
-
-	result.each_with_object({}) do |(key, value), hash|
-		repeated_values = []
-		while (value.scan(/(?<=\[)(.*?)(?=\])/).flatten - repeated_values).present?
-			values = (value.scan(/(?<=\[)(.*?)(?=\])/).flatten - repeated_values)
-			values.each do |key2|
-				value2 = result[key2.downcase.to_sym]
-				value.gsub!("[#{key2}]", value2) if value2 && !repeated_values.include?(key2) && (!value2.index("[#{key2}]"))
-
-				repeated_values << key2 if !value2 || value2.index("[#{key2}]")
-			end
-		end
-		hash[key] = value
-	end
+def make_replaces(text, self_key, fields)
+  text.scan(/\[(.*?)\]/).flatten.each do |entry_name|
+    key = entry_name.downcase.to_sym
+    next if key == self_key || fields[key].nil?
+    entry = make_replaces fields[key], key, fields
+    text.gsub!("[#{entry_name}]", entry)
+  end
+  text
 end
